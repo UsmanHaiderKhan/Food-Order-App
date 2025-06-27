5fbc936fd3276d9efcbc66c141fe35f08293fd66
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-06-27 20:39:07 +0200

    ♻️ Update: add cart display handler to Header component
    Integrates UserProgressContext into Header and adds an onClick handler to the cart button to trigger cart display via userProgressCtx.showCart().

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,16 @@ import logoImg from '../assets/logo.jpg';
 import Button from "./UI/Button.jsx";
 import {useContext} from "react";
 import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return  totalNumberOfItems + item.quantity}, 0);
+
+    function handleShowCart(){
+        userProgressCtx.showCart();
+    }
 
   return (
     <header id="main-header">
@@ -14,7 +20,7 @@ export default function Header() {
           <h1>Order Food</h1>
       </div>
         <nav>
-            <Button textOnly>Cart ({totalCartItems})</Button>
+            <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
         </nav>
     </header>
   );
