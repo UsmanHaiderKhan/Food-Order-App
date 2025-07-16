1e20ff18c878c3042de402f164761b8a508bccef
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-07-17 00:16:58 +0200

    ♻️ Update :  Checkout to use useActionState and useHttp
    Replaces manual form submission and fetch logic with useActionState and a custom useHttp hook for submitting orders. Adds improved loading, error, and success handling, and cleans up the checkout process for better user experience.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,51 +1,92 @@
+import {useContext, useActionState} from "react";
 import Modal from "./UI/Modal.jsx";
 import {currencyFormatter} from "../utils/formatting.js";
-import {useContext} from "react";
 import CartContext from "../store/CartContext.jsx";
 import Input from "./UI/Input.jsx";
 import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import useHttp from "../hooks/useHttp.jsx";
+import Error from "./Error.jsx";
+const requestConfig = {
+    method: "POST",
+    headers:{
+        'Content-Type' : 'application/json'
+    },
 
+};
 export default function Checkout(){
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
+    const {data,
+        // isLoading:isSending,
+        error, sendRequest,
+        clearData} = useHttp('http://localhost:3000/orders', requestConfig,[]);
     const cartTotal = cartCtx.items.reduce((totalPrice,item)=> totalPrice + item.quantity * item.price, 0);
 
     function handleCloseCheckout(){
         userProgressCtx.hideCheckout();
     }
-    function handleSubmit(event){
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const customerData = Object.fromEntries(formData.entries());
-        fetch('http://localhost:3000/orders', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
+    function handleFinish(){
+        userProgressCtx.hideCheckout();
+        cartCtx.clearCart();
+        clearData();
+    }
+    // function handleSubmit(event){
+    //     event.preventDefault();
+    //     const formData = new FormData(event.target);
+    //     const customerData = Object.fromEntries(formData.entries());
+    //     sendRequest(JSON.stringify({
+    //         order:{
+    //             items: cartCtx.items,
+    //             customer: customerData,
+    //         }}));
+    // }
+   async function checkoutAction(prevState, formData){
+            const customerData = Object.fromEntries(formData.entries());
+           await sendRequest(JSON.stringify({
                 order:{
                     items: cartCtx.items,
                     customer: customerData,
-                }
-            }),
-        });
+                }}));
     }
-    return(
+    const [formState, formAction, isSending]= useActionState(checkoutAction, null);
+    let actions= (
+        <>
+            <Button type="button" textOnly onClick={handleCloseCheckout}>Close</Button>
+            <Button>Submit Order</Button>
+        </>
+    );
+    if(isSending){
+        actions = <span>Sending order...</span>
+    }
+    if (data && !error){
+        return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleCloseCheckout}>
+            <h2>Success!</h2>
+            <p>Your order has been submitted successfully!</p>
+            <p>We will process your order in the mentioned time frame further details shared via Email.</p>
+            <p className="modal-actions">
+                <Button onClick={handleFinish}>Okay</Button>
+            </p>
+        </Modal>
+    }
+    return (
         <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleCloseCheckout}>
-            <form onSubmit={handleSubmit}>
-              <h2>Checkout</h2>
+            {/*<form onSubmit={handleSubmit}>*/}
+            <form action={formAction}>
+                <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
                 <Input type="text" label="Full Name" id="name"/>
-                <Input type="email" label="Email Address" id="email" />
-                <Input type="text" label="Street" id="street" />
+                <Input type="email" label="Email Address" id="email"/>
+                <Input type="text" label="Street" id="street"/>
                 <div className="control-row">
-                    <Input type="text" label="City" id="city" />
-                    <Input type="text" label="Postal Code" id="postal-code" />
+                    <Input type="text" label="City" id="city"/>
+                    <Input type="text" label="Postal Code" id="postal-code"/>
                 </div>
+                {error && <Error title="Failed To submit order" message={error} />}
                 <p className="modal-actions">
-                    <Button type="button" textOnly onClick={handleCloseCheckout}>Close</Button>
-                    <Button>Submit Order</Button>
+                    {actions}
                 </p>
             </form>
         </Modal>
-    );
+);
 }
